ff2320b7b00e6715bd6033b70d7c7a3c086a8245
5252HOME <gj1058@naver.com>
2024-10-20 06:54:35 +0900

    share 공유 프론트 추가 - 메인 페이지 북마크 버튼 추가 - 게시글 뷰 페이지 공유 및 북마크 기능 추가 !로그인 안했을 때 이슈 수정 필요 !프론트 제대로 작동 안함

--- src/router/postRouter.jsx
+++ src/router/postRouter.jsx
@@ -8,6 +8,8 @@ const Loading = <Box sx={{display: 'flex'}}><CircularProgress/></Box>
 const postAdd = lazy(() => import("../pages/post/AddPage.jsx"))
 const PostListMy = lazy(() => import("../pages/post/list/ListMyPage.jsx"))
 const PostScrap = lazy(() => import("../pages/post/ScrapPage.jsx"))
+const PostScrap2 = lazy(() => import("../pages/post/ScrapPage2.jsx"))
+
 const PostRead = lazy(() => import("../pages/post/ReadPage.jsx"))
 const PostAddByEditor = lazy(() => import("../pages/post/AddByEditorPage.jsx"))
 const PostModifyByEditor = lazy(() => import("../pages/post/ModifyByEditorPage.jsx"))
@@ -30,8 +32,11 @@ const PostRouter = () => {
         {
             path: 'scrap',
             element: <Suspense fallback={Loading}><PostScrap/></Suspense>
-        }
-        ,
+        },
+        {
+            path: 'scrap2',
+            element: <Suspense fallback={Loading}><PostScrap2/></Suspense>
+        },
         {
             path: 'read/:id',
             element: <Suspense fallback={Loading}><PostRead/></Suspense>
@@ -39,8 +44,7 @@ const PostRouter = () => {
         {
             path: ':id',
             element: <Suspense fallback={Loading}><PostView/></Suspense>
-        }
-        ,
+        },
         {
             path: 'modify/:id',
             element: <Suspense fallback={Loading}><PostModifyByEditor/></Suspense>
