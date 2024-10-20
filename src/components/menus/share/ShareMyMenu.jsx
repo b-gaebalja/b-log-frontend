ffe8cbc2951192f50ea4292eed771e4a86a56fe9
Sung Hyobin <hellmir610@gmail.com>
2024-10-20 17:29:09 +0900

    feat(changes): 세부적인 변경사항들 적용 (#68)
    - 다른 사람들 게시글 보기 메뉴를 내 게시글 목록 메뉴로 수정
    - 나의 스크랩 메뉴를 나의 스크랩 목록 메뉴로 수정
    - 나의 게시글 목록 페이지에서 스크랩 메뉴 삭제

--- src/components/menus/share/ShareMyMenu.jsx
+++ src/components/menus/share/ShareMyMenu.jsx
@@ -6,16 +6,15 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {useNavigate} from "react-router-dom";
 
 const options = [
-  '보기',
-  '수정하기',
-  '스크랩'
+    '보기',
+    '수정하기'
 ];
 
 const ITEM_HEIGHT = 48;
 
 export default function ShareMenu({id}) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
     const navigate = useNavigate(); // useNavigate 훅 사용
 
     const handleClick = (event) => {
@@ -33,44 +32,44 @@ export default function ShareMenu({id}) {
         }
     };
 
-  return (
-      <div>
-        <IconButton
-            aria-label="more"
-            id="long-button"
-            aria-controls={open ? 'long-menu' : undefined}
-            aria-expanded={open ? 'true' : undefined}
-            aria-haspopup="true"
-            style={{color: 'white'}}
-            onClick={handleClick}
-        >
-          {/*<ShareIcon/>*/}
-          <MoreVertIcon/>
-        </IconButton>
-        <Menu
-            id="long-menu"
-            MenuListProps={{
-              'aria-labelledby': 'long-button',
-            }}
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            slotProps={{
-              paper: {
-                style: {
-                  maxHeight: ITEM_HEIGHT * 4.5,
-                  width: '20ch',
-                },
-              },
-            }}
-        >
-          {options.map((option) => (
-              <MenuItem key={option} selected={option === 'Pyxis'}
-                        onClick={() => handleClose(option)}>
-                {option}
-              </MenuItem>
-          ))}
-        </Menu>
-      </div>
-  );
+    return (
+        <div>
+            <IconButton
+                aria-label="more"
+                id="long-button"
+                aria-controls={open ? 'long-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                style={{color: 'white'}}
+                onClick={handleClick}
+            >
+                {/*<ShareIcon/>*/}
+                <MoreVertIcon/>
+            </IconButton>
+            <Menu
+                id="long-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                slotProps={{
+                    paper: {
+                        style: {
+                            maxHeight: ITEM_HEIGHT * 4.5,
+                            width: '20ch',
+                        },
+                    },
+                }}
+            >
+                {options.map((option) => (
+                    <MenuItem key={option} selected={option === 'Pyxis'}
+                              onClick={() => handleClose(option)}>
+                        {option}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </div>
+    );
 }
