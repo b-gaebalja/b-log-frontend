9c997816409b61a929310d002d54a208871317e2
5252HOME <gj1058@naver.com>
2024-10-20 15:58:06 +0900

    config.js, postApi.js 수정 (share)

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,23 +1,23 @@
 import {POST, SHARE} from "./config.js";
-import axios from "axios";
 import jwtAxios from "../util/jwtUtil.jsx";
+import axios from "axios";
 
 export const postRegister = async (registerParam) => {
     const header = {headers: {'Content-Type': 'application/json'}}
 
-    return await axios.post(`${POST}`, registerParam, header)
+    return await jwtAxios.post(`${POST}`, registerParam, header)
 }
 
 export const patchComplete = async (completeParam) => {
     const header = {headers: {'Content-Type': 'application/json'}}
 
-    return await axios.patch(`${POST}`, completeParam, header)
+    return await jwtAxios.patch(`${POST}`, completeParam, header)
 }
 
 export const getPost = async (id) => {
     const header = {headers: {'Content-Type': 'application/json'}}
 
-    return await axios.get(`${POST}/${id}`, header)
+    return await jwtAxios.get(`${POST}/${id}`, header)
 }
 
 export const getMyPosts = async (email, page) => {
@@ -26,7 +26,7 @@ export const getMyPosts = async (email, page) => {
         params: {email: email, pageNumber: page}
     };
 
-    return await axios.get(`${POST}/users`, config);
+    return await jwtAxios.get(`${POST}/users`, config);
 };
 
 export const getUserPosts = async (email, page) => {
@@ -35,7 +35,7 @@ export const getUserPosts = async (email, page) => {
         params: {email: email, pageNumber: page}
     };
 
-    return await axios.get(`${POST}/users`, config);
+    return await jwtAxios.get(`${POST}/users`, config);
 };
 
 export const getAllPosts = async (page) => {
@@ -50,23 +50,28 @@ export const getAllPosts = async (page) => {
 export const patchModify = async (modifyParam) => {
     const header = {headers: {'Content-Type': 'application/json'}}
 
-    return await axios.patch(`${POST}/modify`, modifyParam, header)
+    return await jwtAxios.patch(`${POST}/modify`, modifyParam, header)
 }
 
 // 북마크 추가
-export const addBookmark = async (postId) => {
+export const addBookmark = async (id) => {
+    console.log("북마크 추가 시작");
     const header = { headers: { 'Content-Type': 'application/json' } };
-    return await jwtAxios.post(`${SHARE}/sharePosts/${postId}/shares`, {}, header);
+    console.log("postId : ", id);
+    return await jwtAxios.post(`${SHARE}/${id}/shares`, {}, header);
 };
 
 // 북마크 삭제
 export const removeBookmark = async (postId) => {
+    console.log("북마크 제거 시작");
     const header = { headers: { 'Content-Type': 'application/json' } };
-    return await jwtAxios.delete(`${SHARE}/sharePosts/shares/${postId}`, header);
+    return await jwtAxios.delete(`${SHARE}/delete/${postId}`, header);
 };
 
 // 북마크 목록 가져오기
 export const getBookmarks = async (sharerId) => {
+    console.log("북마크 목록 가져오기 시작");
+    console.log("sharerId: ", sharerId);
     const header = { headers: { 'Content-Type': 'application/json' } };
-    return await jwtAxios.get(`${SHARE}/sharePosts/shares/${sharerId}`, header);
+    return await jwtAxios.get(`${SHARE}/list/${sharerId}`, header);
 };
