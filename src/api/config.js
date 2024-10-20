89852d4b63123fdcc1ae5129d7e59c2f62f5f032
gitdoolgi <gitdoolgi@gmail.com>
2024-10-20 16:57:37 +0900

    fix : config comment, notification 추가

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,12 @@
-export const API_SERVER_HOST = 'http://43.200.154.136:8080'
+export const API_SERVER_HOST = 'https://b-log.duckdns.org'
 
 export const POST = `${API_SERVER_HOST}/posts`
 
 export const USER = `${API_SERVER_HOST}/users`
 
 export const SHARE = `${API_SERVER_HOST}/share`
+
+export const COMMENT = `${API_SERVER_HOST}/comment`
 
 export const NOTIFICATION = `${API_SERVER_HOST}/notification`
 
